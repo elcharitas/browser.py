d640774e20318ce4b8544416faa56e44bc004457
phores <jonathanirhodia@gmail.com>
2021-10-04 13:31:13 +0100

    info: add example logic

--- browser.py
+++ browser.py
@@ -91,7 +91,7 @@ class Browser(QApplication):
         
         Example:
             app.show_popup(message='Some Message')
-        """        
+        """
         messageBox = QMessageBox()
         messageBox.setText(message)
         messageBox.exec_()
@@ -153,5 +153,9 @@ class Browser(QApplication):
 
 
 app = Browser()
+# additional browser logic can come after this line
+"""Example logic to show popup with the text box's text
+app.show_popup(message=app.window.textbox)
+"""
 # this should always be called last
 exit(app.exec_())
