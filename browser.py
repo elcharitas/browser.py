d6399d517267a8eb76cb2db33611a7c1ca63cada
phores <jonathanirhodia@gmail.com>
2021-10-13 17:16:21 +0100

    feat: remove menubar and dead code

--- browser.py
+++ browser.py
@@ -1,30 +1,23 @@
 # import core functionalities
-from sys import argv, exit
+from sys import argv
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QIcon
 from screeninfo import get_monitors
 
-# import required PyQt5 Widgtes
+# import required PyQt5 Widgets
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
-    QLineEdit,
     QMessageBox,
-    QPushButton,
-    QWidget,
-    QVBoxLayout,
-    QHBoxLayout
 )
 from PyQt5.QtWebEngineWidgets import QWebEngineSettings, QWebEngineView
 
 # Browser class to create new browser application and window
-
-
 class Browser(QApplication):
     # list of autodetected screens
     screens = get_monitors()
 
-    def __init__(self, URL=argv[1] if argv[1:] else 'https://google.com', half_screen=False):
+    def __init__(self, entry = None, URL=argv[1] if argv[1:] else 'https://google.com', half_screen=False):
         # initialize the application
         super(QApplication, self).__init__([])
 
@@ -35,6 +28,7 @@ class Browser(QApplication):
         # create a window and load it
         self.window = self.Window(half_screen)
         self.change_url(URL)
+        entry(self)
 
     def has_screen(self, screen):
         """Used internally to check if a screen is supported or existing
@@ -44,8 +38,16 @@ class Browser(QApplication):
 
         Returns:
             bool: true if the screen exists
-        """        
+        """
         return screen <= len(self.screens) and screen > 0
+    
+    def show_half_screen(self, show=True):
+        """Dynamically shows half/full screen.
+
+        Args:
+            show (bool, optional): whether or not half screen should be displayed. Defaults to True.
+        """        
+        self.window.resolve_screen(show)
     
     def change_url(self, url: str):
         """Changes the URL the engine is to load
@@ -56,7 +58,7 @@ class Browser(QApplication):
         Example:
             app = Browser('https://example.com')
             app.change_url('https://example2.com')
-        """        
+        """
         self.window.engine.setUrl(QUrl(url))
     
     def hide_menubar(self, disabled=True):
@@ -68,7 +70,7 @@ class Browser(QApplication):
         Example:
             app = Browser('https://example.com')
             app.hide_menubar()
-        """        
+        """
         if disabled is not False:
             self.window.menubar.show()
         else:
@@ -89,46 +91,18 @@ class Browser(QApplication):
 
     class Window(QMainWindow):
         def __init__(self, half_screen: bool):
-            # create a connnection
+            # create a connection
             super(QMainWindow, self).__init__()
-
             self.browser = Browser.instance()
-
-            # create a container and its layout
-            self.container = QWidget()
-            self.layout = QVBoxLayout()
-            self.container.setLayout(self.layout)
-            self.setCentralWidget(self.container)
 
             # create the engine to bind
             self.engine = QWebEngineView()
-            self.engine.settings().setAttribute(QWebEngineSettings.WebAttribute.ShowScrollBars, False)
-            self.engine.settings().setAttribute(QWebEngineSettings.WebAttribute.AllowRunningInsecureContent, True)
-            self.layout.addWidget(self.engine)
+            #self.engine.settings().setAttribute(QWebEngineSettings.WebAttribute.ShowScrollBars, False)
+            self.engine.settings().setAttribute(
+                QWebEngineSettings.WebAttribute.AllowRunningInsecureContent, True)
+            self.setCentralWidget(self.engine)
 
-            # create the menubar
-            menubar = QHBoxLayout()
-
-            # create refresh button and add to layout
-            ButtonH = QPushButton('Refresh', self)
-            ButtonH.pressed.connect(self.refresh_browser)
-            menubar.addWidget(ButtonH)
-
-            # create extra button
-            Button2 = QPushButton('Button 2', self)
-            menubar.addWidget(Button2)
-
-            # add the textbox
-            self.TextBox = QLineEdit()
-            menubar.addWidget(self.TextBox)
-
-            # add the widget to layout
-            self.menubar = QWidget()
-            self.menubar.setLayout(menubar)
-            self.menubar.setMaximumHeight(40)
-            self.layout.addWidget(self.menubar)
-            
-            # display the appropiate screen
+            # display the appropriate screen
             self.resolve_screen(half_screen)
         
         def refresh_browser(self):
@@ -140,20 +114,11 @@ class Browser(QApplication):
             """Resolves the screen/monitor
 
             Args:
-                screen (int): The appropiate screen number
-            """            
+                screen (int): The appropriate screen number
+            """
             if half_screen:
                 self.setFixedWidth(self.browser.screens[-1].width/2)
-                self.setFixedHeight(self.browser.screens[-1].height/2)
+                self.setFixedHeight(self.browser.screens[-1].height)
+                self.show()
             else:
                 self.showFullScreen()
-        
-        @property
-        def textbox(self):
-            """Get the text within the textbox as a variable
-            
-            Example:
-                app = Browser('https://example.com')
-                print(app.window.textbox)
-            """
-            return self.TextBox.text()
